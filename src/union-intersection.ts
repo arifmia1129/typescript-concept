49e4ee13a69ef83032fdbb42797fc2a2a3543203
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-18 05:13:06 +0600

    union intersection

---
+++ src/union-intersection.ts
@@ -0,0 +1,23 @@
+// intersection
+type StudentTypeA = {
+    name:string;
+    age:number;
+};
+
+type StudentTypeB = StudentTypeA & {
+    roll:number;
+    institute:string
+};
+
+const studentInfo1:StudentTypeB = {
+    name:"Arif",
+    age:22,
+    roll:112864,
+    institute:"VTTI"
+};
+
+// union
+const studentInfo2:StudentTypeA | StudentTypeB = {
+    name:"Arif",
+    age:20
+}
