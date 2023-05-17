9334ae8510673196394496911a4ef25b8ba3e84a
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-18 05:45:20 +0600

    null unknown

---
+++ src/null-unknown.ts
@@ -0,0 +1,22 @@
+// null
+const searchUserByName = (name:string | null) =>{
+    if(name === null) {
+        console.log("Nothing for search!");
+    } else{
+        console.log(`Search by name is: ${name}`);
+    }
+}
+
+// searchUserByName("Arif");
+
+
+// unknown
+const guessSomething = (guess:unknown)=>{
+    console.log(guess);
+}
+
+guessSomething("Arif");
+guessSomething(123);
+guessSomething(null);
+guessSomething(undefined);
+guessSomething(true);
