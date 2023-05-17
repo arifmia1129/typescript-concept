9ae1329928c1cfaaa4d30509250ece507bef049e
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-17 07:06:35 +0600

    tuple

--- src/array-tuples.ts
+++ src/array-tuples.ts
@@ -2,3 +2,6 @@
 const names:string[] = ["Binu", "Arif"];
 const age:number[] = [21, 22];
 const person:(string|number|boolean)[] =["Arif",112864, true];
+
+// tuple
+const studentInfo:[string, number, string] = ["Arif", 112864, "VTTI"];
