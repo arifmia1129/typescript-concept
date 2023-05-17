cabdb2103c2e04de9d754168880f85f671ab3ff1
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-17 07:50:19 +0600

    spread operator, rest parameter

---
+++ src/spread-rest.ts
@@ -0,0 +1,19 @@
+// spread operator
+const names1:string[] = ["Arif", "Binu"];
+const names2:string[] = ["Nazrul", "Nurani"];
+
+names1.push(...names2);
+
+console.log(names1);
+
+// rest parameter
+const collectScore =(...score:number[])=>{
+    let sum = 0;
+    score.forEach(s => {
+        sum = sum + s;
+    })
+
+    return sum;
+}
+
+console.log(collectScore(1, 2,3,4,5));
