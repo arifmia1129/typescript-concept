518279bdbf3a4587f90ad88d988f23375d7789db
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-18 04:57:40 +0600

    interface

---
+++ src/interface.ts
@@ -0,0 +1,14 @@
+interface IStudent {
+    name:string;
+    age:number
+};
+
+const student3:IStudent = {
+    name:"Arif",
+    age:22
+}
+
+const student4:IStudent = {
+    name:"Binu",
+    age:21
+}
