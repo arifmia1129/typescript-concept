c2e453bf0880fa462edc9045983fe92031f787d9
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-18 05:51:30 +0600

    ternary operator

---
+++ src/ternary.ts
@@ -0,0 +1,11 @@
+const isPassedExam:boolean = true;
+
+// if(isPassedExam) {
+//     console.log("Congratulation for passing examination!");
+// }else{
+//     console.log("No problem! Try again! You can do!")
+// }
+
+const checkPassed = isPassedExam ? "Congratulation for passing examination!" : "No problem! Try again! You can do!";
+
+console.log(checkPassed);
