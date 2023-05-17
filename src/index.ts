2ca07f9b393204346687c2702f86ea7827cedc4f
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-17 06:54:40 +0600

    primitives data types

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// Primitives Data Type in Typescript
+/**
+ * String
+ * Number
+ * Boolean
+ * Null
+ * Undefined
+*/
+
+const student:string = "Arif";
+const roll:number = 112864;
+const isPassed:boolean=true;
