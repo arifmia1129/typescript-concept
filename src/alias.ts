8312daad60ed24b48d25593e45ef724fa19cb042
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-18 04:50:59 +0600

    type alias

---
+++ src/alias.ts
@@ -0,0 +1,30 @@
+type IncomeType = number | string;
+
+const myIncome = "Alhamdulillah valoi cholche.";
+const herIncome = 40000;
+
+type StudentType = {
+    name:string;
+    age:number;
+    roll:number;
+    institute:string;
+    isPassed:boolean;
+};
+
+
+const student1:StudentType = {
+    name:"Binu",
+    age:21,
+    roll:111111,
+    institute:"GMC",
+    isPassed:true
+};
+
+const student2:StudentType = {
+    name:"Arif",
+    age:22,
+    roll:112864,
+    institute:"VTTI",
+    isPassed:true
+};
+
