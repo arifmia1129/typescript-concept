40db46ee916a971e3ba4eb91d825c9c0d1343276
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-20 05:50:08 +0600

    mapped

---
+++ src/mapped.ts
@@ -0,0 +1,22 @@
+const numbersArr:number[] = [1, 2, 3, 4, 5];
+
+const stringArr:string[] = numbersArr.map(num => num.toString());
+
+console.log(stringArr);
+
+type FriendsType = {
+    friend1:string;
+    friend2:string;
+    friend3:string;
+};
+
+type FriendsTypeReadOnly = {
+  readonly [key in keyof FriendsType] :FriendsType[key];
+};
+
+const myFriends:FriendsTypeReadOnly = {
+    friend1:"Binu",
+    friend2:"Nurani",
+    friend3:"Shahin"
+}
+
