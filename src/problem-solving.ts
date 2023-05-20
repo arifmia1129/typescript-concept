d60360f261e5d35347628e7bc0210d5e4624f2d4
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-20 07:48:02 +0600

    solved problem 5

--- src/problem-solving.ts
+++ src/problem-solving.ts
@@ -11,13 +11,13 @@
 
 // 2.Define an interface called Person that has the following properties: name (string), age (number), and email (string). Create an object based on this interface and print out its properties.
 
-// interface Person{
+// interface IPerson{
 //     name:string;
 //     age:number;
 //     email:string
 // }
 
-// const person = {
+// const person:IPerson = {
 //     name:"Arif",
 //     age:21,
 //     email:"arif.vtti@gmail.com"
@@ -37,15 +37,55 @@
 
 // 4. Implement a generic function called reverseArray that takes an array of any type and returns the reversed version of the array. Test it with different types of arrays (e.g., numbers, strings) to ensure it works correctly.
 
-const reverseArr =<T> (arr:T[]):T[] => {
-    let newArr = [];
-    for(let i = arr.length-1; i >= 0; i--){
-        newArr.push(arr[i]);
-    }
-    return newArr;
-} 
+// const reverseArr =<T> (arr:T[]):T[] => {
+//     let newArr = [];
+//     for(let i = arr.length-1; i >= 0; i--){
+//         newArr.push(arr[i]);
+//     }
+//     return newArr;
+// } 
 
 
-console.log(reverseArr(["arif", "binu"]));
+// console.log(reverseArr(["arif", "binu"]));
+
+
+// 5. Create a generic interface called Repository that represents a generic data repository. It should have methods like getAll, getById, create, update, and delete. Define the types for the methods and create a class that implements this interface.
+
+interface IRepository<T, U, X, Y,Z> {
+    getAll():T;
+    getById():U;
+    create():X;
+    update():Y;
+    delete():Z;
+}
+
+
+class RestApi implements IRepository<Array<object>, object, object, object, object> {
+    getAll(){
+        return [
+            {name:"Arif"}
+        ]
+    };
+    getById():object{
+        return {
+            success:true
+        }
+    };
+    create():object{
+        return {
+            success:true
+        }
+    };
+    update():object{
+        return {
+            success:true
+        }
+    };
+    delete():object{
+        return {
+            success:true
+        }
+    };
+}
 
 
