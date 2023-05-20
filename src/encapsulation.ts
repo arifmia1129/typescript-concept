cca04a393a92b6de0d60560f3a5632fe1d7aa233
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-20 07:08:17 +0600

    encapsulation

---
+++ src/encapsulation.ts
@@ -0,0 +1,13 @@
+class Bank {
+    id:number;
+    user:string;
+   private balance:number;
+
+    constructor(id:number, user:string, balance:number) {
+        this.id = id;
+        this.user = user;
+        this.balance = balance;
+    }
+}
+
+const myAccount = new Bank(101, "Arif", 100000);
