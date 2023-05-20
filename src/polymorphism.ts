dbc64dd79418a7b43d2b5d988626d135a18a0356
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-20 06:51:41 +0600

    polymorphism

---
+++ src/polymorphism.ts
@@ -0,0 +1,19 @@
+class Father {
+    showInfo() {
+        console.log(`Father`);
+        
+    }
+}
+class Son1 extends Father {
+    showInfo() {
+        console.log(`son 1`);
+        
+    }
+}
+
+class Son2 extends Father {
+    showInfo() {
+        console.log(`son 2`);
+        
+    }
+}
