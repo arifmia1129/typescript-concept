602937ee93470b186bccb30fa4d21335f12f90ef
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-20 07:01:28 +0600

    abstraction

---
+++ src/abstraction.ts
@@ -0,0 +1,35 @@
+// abstract class
+abstract class Car {
+    abstract start():void;
+    abstract stop():void;
+}
+
+class MyCar extends Car {
+    start(): void {
+        console.log("Engine starting....");
+        
+    }
+    stop(): void {
+        console.log("Engine stoping....");
+        
+    }
+}
+
+// interface 
+
+interface IVehicle {
+    start():void;
+    stop():void;
+}
+
+class Bus implements IVehicle {
+    start(): void {
+        console.log("Engine start");
+        
+    }
+
+    stop(): void {
+        console.log("Engine stop");
+        
+    }
+}
