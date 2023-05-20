c7a8149c16e8af16b3fc3addc8b49d35c259ed7c
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-20 06:08:19 +0600

    inheritance

--- src/oop.ts
+++ src/oop.ts
@@ -13,4 +13,20 @@ class Person{
 
 }
 
+
+// inheritance
+class Employee extends Person{
+    designation:string;
+    salary:number;
+
+    constructor(name:string, age:number, gender:string,designation:string, salary:number) {
+        super(name, age, gender);
+        this.designation = designation;
+        this.salary = salary;
+    }
+}
+
+
+
 const person1 = new Person("Arif", 22, "male");
+const person2 = new Employee("Arif", 22, "male", "Developer", 2000000);
