69a7c6d5b77911bab20b9583d90cb73cf8e91456
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-21 07:49:23 +0600

    solved practice problem 2 & 3

--- src/practice-problem.ts
+++ src/practice-problem.ts
@@ -3,3 +3,20 @@
 
 // Solution-1:
 // const userInfo:[number, string, string, boolean, undefined, string] = [101, "Ema", "John", true,  , "2023"];
+
+
+// 2. Write a TypeScript function that takes in two arrays of numbers as parameters. The function should compare the elements in both arrays and return a new array that contains only the elements that are present in both arrays.
+
+// solution-2
+
+// const compareElements =<T> (param1:Array<T>, param2:Array<T>):T[]=> {
+//     let newArr:T[] = [];
+//     param1.forEach(el => {
+//         if(param2.includes(el)){
+//             newArr.push(el);
+//         }
+//     })
+//     return newArr;
+// }
+
+// console.log(compareElements<number>([1, 2, 3], [1, 2]));
